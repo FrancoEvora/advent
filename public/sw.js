9c753de17f8fb45cb19c5e4992843f747125cf02
FrancoEvora <164817558+FrancoEvora@users.noreply.github.com>
2026-07-19 11:35:18 -0300

    Renova cache da PWA para ERP v3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "evora-gestao-erp-v2";
+const CACHE = "evora-gestao-erp-v3";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
